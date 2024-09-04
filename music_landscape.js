ba35b4f6b5a7b97a5b26bb677229b2bb0b23b19b
Robbie <robbiek3214@gmail.com>
2024-09-04 13:32:34 +1200

    a

--- music_landscape.js
+++ music_landscape.js
@@ -1,59 +1,59 @@
-let last_words = "";
-let last_words_opacity = 0;
+let voiceThreshold = false;
 
 function draw_one_frame(words, vocal, drum, bass, other,counter) {
-  background(255,236,180); // cream
-  fill(244,161,39); // orange
+  
+  fill(244,161,39,50); // orange
+  //noFill();
+  //stroke(0)
 
-  let stripeWidth = map(other, 40, 100, 40, 80, true);
+  if (vocal >= 45){
+    voiceThreshold = true
+} else {
+    voiceThreshold = false
+}
+
+     let seconds = Math.floor(counter / 60);
+
+     if (seconds < 10) {
+        background(0,0,0);
+    } else if (seconds < 20){
+        background(255,236,180);
+    } else {
+        background (255,255,255)
+    }
+
+ let stripeWidth = map(other, 40, 100, 40, 80, true);
 
   let numStripes = height / stripeWidth;
   for(let i=0; i<numStripes; i=i+2) {
+    // let i=0; i<numStripes; i=i+2
+    // let i = numStripes - 1; i >= 0; i = i - 2
     let cury = map(i, 0, numStripes-1, 0, height);
-    rect(0, cury, width, stripeWidth);
+    //rect(0, cury, width, stripeWidth);
+    circle(canvasWidth/2, canvasHeight/2, cury* 0.8)
   }
 
-  let triangleHeight = map(bass, 40, 100, 200, 550, true);
-  fill(117,200,174); // teal
-  for(let i=0; i<3; i++) {
-    let cur_x = map(i, 0, 4, 0, width);
-    let next_x = map(i+1, 0, 3, 0, width);
-    let mid_x = (cur_x + next_x) / 2.0;
-    //let cur_y = 4 * height / 5;
-    let cur_y = height 
-    triangle(cur_x, cur_y, mid_x, cur_y - triangleHeight, next_x, cur_y);
-  }
-
-
-  let drumSize = map(drum, 30, 100, 30, 300, true);
-  fill(90,61,43); // brown
-  rect(0, 0, drumSize, drumSize);
-  rect(width, 0, -drumSize, drumSize);
-  rect(0, height, drumSize, -drumSize);
-  rect(width, height, -drumSize, -drumSize);
-
-  let ovalPlace = map(vocal, 20, 100, height-50, 50, true);
+  let ovalPlace = map(vocal, 45, 100, height-50, 50, true);
   let ovalSize = map(vocal, 20, 100, 60, 150, true);
-  fill(229,119,30); // darker orange
-  ellipse(width/2, ovalPlace, ovalSize);
 
-  if(words == "") {
-    last_words_opacity = last_words_opacity * 0.95;
-    words = last_words;
+  if (voiceThreshold){
+    fill(229,119,30); // darker orange
+    ellipse(width/2, ovalPlace, ovalSize);
   }
-  else {
-    last_words_opacity = (1 + last_words_opacity) * 1.1;
-    if(last_words_opacity > 255) {
-      last_words_opacity = 255;
-    }
-  }
-  last_words = words;
 
-  textFont('Georgia');
+
+  
+
+
+  fill (255)
+  stroke(0)
+  strokeWeight(4)
+  textFont('Cascadia Code');
   textAlign(CENTER);
   textStyle(BOLD);
-  textSize(80);
-  noStroke();
-  fill(0, 0, 0, int(last_words_opacity));
-  text(words, width/2, height/2);
+  textSize(40);
+  text (seconds, 200, 200)
+  text (voiceThreshold, 200, 300)
+  noStroke()
+
 }
--- music_starter.js
+++ music_starter.js
@@ -1,47 +1,15 @@
 
-let scene;
-let firstRun = true;
-
-let rectY = 100;
-
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  // if (firstRun){
-  //   scene = loadImage ('assets/scene.png');
-  //   firstRun = false;
-  // }
-
   background(20)
-  //image(scene, 100,100);
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
- 
-
-  let counterFrac = Math.floor (counter / 16);
-  text (counterFrac, 100,100);
-
-  if (counterFrac % 2 == 0){
-    fill(255)
-    stroke(255,255,100);
-    
-  } else {
-    noFill();
-    noStroke();
-
-    rectY+=10;
-    if (rectY > canvasHeight){
-      rectY = 100;
-    }
-
-  }
-
-  rect(100,rectY,100);
-
-  let bar_spacing = height / 10;
-  let bar_height = width / 12;
-  let bar_pos_x = width / 2;
   
+   let bar_spacing = height / 10;
+   let bar_height = width / 12;
+   let bar_pos_x = width / 2;
+ 
 
    // vocal bar is red
    fill(200, 0, 0);
@@ -72,73 +40,4 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    textAlign(CENTER);
    textSize(vocal);
    text(words, width/2, height/3);
-}
-
-
-
-// let last_words = "";
-// let last_words_opacity = 0;
-
-function draw_one_frame(words, vocal, drum, bass, other,counter) {
-
-  // store colours as variables and demonstrate array in 2D
-  // add alpha to the orange colour
-  background(255,236,180); // cream
-  fill(244,161,39); // orange
-
-// replace as ringWidth, refactor rect to draw circles of increasing size
-  let stripeWidth = map(other, 40, 100, 40, 80, true);
-
-  let numStripes = height / stripeWidth;
-  for(let i=0; i<numStripes; i=i+2) {
-    let cury = map(i, 0, numStripes-1, 0, height);
-    rect(0, cury, width, stripeWidth);
-  }
-
-  // get rid of this
-  let triangleHeight = map(bass, 40, 100, 200, 550, true);
-  fill(117,200,174); // teal
-  for(let i=0; i<3; i++) {
-    let cur_x = map(i, 0, 4, 0, width);
-    let next_x = map(i+1, 0, 3, 0, width);
-    let mid_x = (cur_x + next_x) / 2.0;
-    //let cur_y = 4 * height / 5;
-    let cur_y = height 
-    triangle(cur_x, cur_y, mid_x, cur_y - triangleHeight, next_x, cur_y);
-  }
-
-  // get rid of this
-  let drumSize = map(drum, 30, 100, 30, 300, true);
-  fill(90,61,43); // brown
-  rect(0, 0, drumSize, drumSize);
-  rect(width, 0, -drumSize, drumSize);
-  rect(0, height, drumSize, -drumSize);
-  rect(width, height, -drumSize, -drumSize);
-
-  // explicit if statement for voiceThreshold
-  let ovalPlace = map(vocal, 20, 100, height-50, 50, true);
-  let ovalSize = map(vocal, 20, 100, 60, 150, true);
-  fill(229,119,30); // darker orange
-  ellipse(width/2, ovalPlace, ovalSize);
-
-  // get rid of this all
-  if(words == "") {
-    last_words_opacity = last_words_opacity * 0.95;
-    words = last_words;
-  }
-  else {
-    last_words_opacity = (1 + last_words_opacity) * 1.1;
-    if(last_words_opacity > 255) {
-      last_words_opacity = 255;
-    }
-  }
-  last_words = words;
-
-  textFont('Georgia');
-  textAlign(CENTER);
-  textStyle(BOLD);
-  textSize(80);
-  noStroke();
-  fill(0, 0, 0, int(last_words_opacity));
-  text(words, width/2, height/2);
 }
